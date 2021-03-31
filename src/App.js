22a98afaeceecdb8a89fc1b86c13fcea8492a791
Venkateshe <kolapallinagavenkatesh@gmail.com>
2021-03-31 13:22:58 +0530

    Added Styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Splash from "./Components/SplashScreen";
+import { ThemeProvider } from "styled-components";
+
+
+const LightTheme = {
+  pageBackground: "white",
+  titleColor: "#dc658b",
+  tagLineColor: "black"
+};
+
+const DarkTheme = {
+  pageBackground: "#282c36",
+  titleColor: "lightpink",
+  tagLineColor: "lavender"
+}
+
+const themes = {
+  light: LightTheme,
+  dark: DarkTheme,
+}
 
 function App() {
   return (
